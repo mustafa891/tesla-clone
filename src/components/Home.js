baee589c776490bee6459b81aabb272c51c6be4e
mustafa891 <mustafa.fakher090@gmail.com>
2023-03-05 22:27:09 +0300

    ADD: fullpage scroll for app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,60 +1,88 @@
 import React from 'react'
 import styled from "styled-components";
 import Section from "./Section"
+import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage';
 
 export default function Home() {
- return <Container>
-    <Section 
-     title="Model 3"
-     description="Leasing starting at $349/mo"
-     leftBtnText="Custom Order"
-     rightBtnText="Demo Drive"
-     backgroundImg="model-3.jpg"
-    />
-      <Section 
-     title="Model Y"
-     description=""
-     leftBtnText="Custom Order"
-     rightBtnText="Demo Drive"
-     backgroundImg="model-y.jpg"
-    />
-      <Section 
-     title="Model S"
-     description="Schedule a Demo Drive"
-     leftBtnText="Custom Order"
-     rightBtnText="View Inventory"
-     backgroundImg="model-s.jpg"
-    />
-      <Section 
-     title="Model X"
-     description="Schedule a Demo Drive"
-     leftBtnText="Custom Order"
-     rightBtnText="View Inventory"
-     backgroundImg="model-x.jpg"
-    />
-       <Section 
-     title="Solar Panels"
-     description="Lowest Cost Solar Panels in America"
-     leftBtnText="Order Now"
-     rightBtnText="Learn More"
-     backgroundImg="solar-panel.jpg"
-    />
-       <Section 
-        title="Solar Roof"
-        description="Produce Clean Energy From Your Roof"
-        leftBtnText="Order Now"
-        rightBtnText="Learn More"
-        backgroundImg="solar-roof.jpg"
-    />
-       <Section 
-        title="Accessories"
-        description=""
-        leftBtnText="Shop Now"
-        backgroundImg="accessories.jpg"
-    />
- </Container>
+ return (
+   <Container>
+         <Fullpage>
+            <FullPageSections>
+               <FullpageSection>
+                  <Section 
+                  title="Model 3"
+                  description="Leasing starting at $349/mo"
+                  leftBtnText="Custom Order"
+                  rightBtnText="Demo Drive"
+                  backgroundImg="model-3.jpg"
+                  />
+               </FullpageSection>
+
+               <FullpageSection>
+                  <Section 
+                  title="Model Y"
+                  description=""
+                  leftBtnText="Custom Order"
+                  rightBtnText="Demo Drive"
+                  backgroundImg="model-y.jpg"
+                  />
+               </FullpageSection>
+
+               <FullpageSection>
+                  <Section 
+                  title="Model S"
+                  description="Schedule a Demo Drive"
+                  leftBtnText="Custom Order"
+                  rightBtnText="View Inventory"
+                  backgroundImg="model-s.jpg"
+                  />
+               </FullpageSection>
+   
+               <FullpageSection>
+                  <Section 
+                  title="Model X"
+                  description="Schedule a Demo Drive"
+                  leftBtnText="Custom Order"
+                  rightBtnText="View Inventory"
+                  backgroundImg="model-x.jpg"
+                  />
+               </FullpageSection>
+               
+               <FullpageSection>
+                  <Section 
+                  title="Solar Panels"
+                  description="Lowest Cost Solar Panels in America"
+                  leftBtnText="Order Now"
+                  rightBtnText="Learn More"
+                  backgroundImg="solar-panel.jpg"
+                  />
+               </FullpageSection>
+               
+               <FullpageSection>
+                  <Section 
+                  title="Solar Roof"
+                  description="Produce Clean Energy From Your Roof"
+                  leftBtnText="Order Now"
+                  rightBtnText="Learn More"
+                  backgroundImg="solar-roof.jpg"
+                  />
+               </FullpageSection>
+            
+               <FullpageSection>
+                  <Section 
+                  title="Accessories"
+                  description=""
+                  leftBtnText="Shop Now"
+                  backgroundImg="accessories.jpg"
+                  />
+               </FullpageSection>
+               
+            </FullPageSections>
+         </Fullpage>
+   </Container>
+ )
 }
 
 const Container = styled.div`
-overflow-x: hidden;
+position: relative;
 `
